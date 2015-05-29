13a954d1427a33df064096e2c8daf1dc29b4fb03
liuzhao.wei <liuzhao.wei@chukong-inc.com>
2015-05-29 19:38:24 +0800

    Optimize code

--- src/com/loader/dexloader/LoaderApp.java
+++ src/com/loader/dexloader/LoaderApp.java
@@ -24,122 +24,19 @@ import dalvik.system.DexClassLoader;
 
 public class LoaderApp extends Application {
 
-    private static final String appkey = "APPLICATION_CLASS_NAME";
+    private static final String APPLICATION_KEY = "APPLICATION_CLASS_NAME";
     private static final String LODER_CONFIG_FILE = "loaderconfig.dat";
-    private String mEncryptionDexFile = "classes.dex";
-    private DexClassLoader mDexClassLoader;
+    private static final String APP_DEX_PATH = "dex_path";
+    private String mEncryptionJarFile = "_dex_data";
+    private String mDestJarFile = "_dex_data.jar";
     private Application mApp;
 
-    /*
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        try {
-            String odexPath = getDir("odex_path", MODE_PRIVATE)
-                    .getAbsolutePath();
-            parseLoaderConfig();
-            String dexPath = generateSrcDex();
-            String libPath = getApplicationInfo().nativeLibraryDir;
-            Log.d(Log.TAG, "dexPath : " + dexPath);
-            Log.d(Log.TAG, "libPath : " + libPath);
-            Log.d(Log.TAG, "odexPath : " + odexPath);
-            if (TextUtils.isEmpty(dexPath)) {
-                Log.d(Log.TAG, "Fail to Write " + mEncryptionDexFile);
-                System.exit(0);
-            }
-            // 配置动态加载环境
-            Object currentActivityThread = RefInvoke.invokeStaticMethod(
-                    "android.app.ActivityThread", "currentActivityThread",
-                    new Class[] {}, new Object[] {});
-            String packageName = this.getPackageName();
-            HashMap mPackages = (HashMap) RefInvoke.getFieldOjbect(
-                    "android.app.ActivityThread", currentActivityThread,
-                    "mPackages");
-            WeakReference wr = (WeakReference) mPackages.get(packageName);
-            mDexClassLoader = new DexClassLoader(dexPath, odexPath,
-                    libPath, (ClassLoader) RefInvoke.getFieldOjbect(
-                            "android.app.LoadedApk", wr.get(), "mClassLoader"));
-            RefInvoke.setFieldOjbect("android.app.LoadedApk", "mClassLoader",
-                    wr.get(), mDexClassLoader);
-
-        } catch (Exception e) {
-            Log.d(Log.TAG, "error : " + e);
-        }
-    }*/
-
-    /*
     public void onCreate() {
         super.onCreate();
-        // 如果源应用配置有Appliction对象，则替换为源应用Applicaiton，以便不影响源程序逻辑。
-        String appClassName = null;
-        try {
-            ApplicationInfo ai = this.getPackageManager().getApplicationInfo(
-                    this.getPackageName(), PackageManager.GET_META_DATA);
-            Bundle bundle = ai.metaData;
-            if (bundle != null && bundle.containsKey("APPLICATION_CLASS_NAME")) {
-                appClassName = bundle.getString("APPLICATION_CLASS_NAME");
-            } else {
-                return;
-            }
-        } catch (NameNotFoundException e) {
-            Log.d(Log.TAG, "error : " + e);
-        }
-
-        Object currentActivityThread = RefInvoke.invokeStaticMethod(
-                "android.app.ActivityThread", "currentActivityThread",
-                new Class[] {}, new Object[] {});
-        Object mBoundApplication = RefInvoke.getFieldOjbect(
-                "android.app.ActivityThread", currentActivityThread,
-                "mBoundApplication");
-        Object loadedApkInfo = RefInvoke.getFieldOjbect(
-                "android.app.ActivityThread$AppBindData", mBoundApplication,
-                "info");
-        RefInvoke.setFieldOjbect("android.app.LoadedApk", "mApplication",
-                loadedApkInfo, null);
-        Object oldApplication = RefInvoke.getFieldOjbect(
-                "android.app.ActivityThread", currentActivityThread,
-                "mInitialApplication");
-        ArrayList<Application> mAllApplications = (ArrayList<Application>) RefInvoke
-                .getFieldOjbect("android.app.ActivityThread",
-                        currentActivityThread, "mAllApplications");
-        mAllApplications.remove(oldApplication);
-        ApplicationInfo applicationInfo = (ApplicationInfo) RefInvoke
-                .getFieldOjbect("android.app.LoadedApk", loadedApkInfo,
-                        "mApplicationInfo");
-        ApplicationInfo appInfoInAppBindData = (ApplicationInfo) RefInvoke
-                .getFieldOjbect("android.app.ActivityThread$AppBindData",
-                        mBoundApplication, "appInfo");
-        applicationInfo.className = appClassName;
-        appInfoInAppBindData.className = appClassName;
-        Application app = (Application) RefInvoke.invokeMethod(
-                "android.app.LoadedApk", "makeApplication", loadedApkInfo,
-                new Class[] { boolean.class, Instrumentation.class },
-                new Object[] { false, null });
-        RefInvoke.setFieldOjbect("android.app.ActivityThread",
-                "mInitialApplication", currentActivityThread, app);
-
-        HashMap mProviderMap = (HashMap) RefInvoke.getFieldOjbect(
-                "android.app.ActivityThread", currentActivityThread,
-                "mProviderMap");
-        Iterator it = mProviderMap.values().iterator();
-        while (it.hasNext()) {
-            Object providerClientRecord = it.next();
-            Object localProvider = RefInvoke.getFieldOjbect(
-                    "android.app.ActivityThread$ProviderClientRecord",
-                    providerClientRecord, "mLocalProvider");
-            RefInvoke.setFieldOjbect("android.content.ContentProvider",
-                    "mContext", localProvider, app);
-        }
-        Log.d(Log.TAG, "appClassName : " + appClassName);
-        Log.d(Log.TAG, "app : " + app);
-        app.onCreate();
-    }*/
-    public void onCreate() {
-        super.onCreate();
-        setApkClassLoader();
         try {
             String str2 = getPackageManager().getApplicationInfo(
                     getPackageName(), PackageManager.GET_META_DATA).metaData
-                    .getString(appkey);
+                    .getString(APPLICATION_KEY);
             mApp = ((Application) getClassLoader().loadClass(str2)
                     .newInstance());
             Method localMethod = Class
@@ -155,9 +52,11 @@ public class LoaderApp extends Application {
         }
     }
 
-    public void setApkClassLoader() {
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
         try {
-            String odexPath = getDir("odex_path", MODE_PRIVATE)
+            String odexPath = getDir(APP_DEX_PATH, MODE_PRIVATE)
                     .getAbsolutePath();
             parseLoaderConfig();
             String dexPath = generateSrcDex();
@@ -166,30 +65,36 @@ public class LoaderApp extends Application {
             Log.d(Log.TAG, "libPath : " + libPath);
             Log.d(Log.TAG, "odexPath : " + odexPath);
             if (TextUtils.isEmpty(dexPath)) {
-                Log.d(Log.TAG, "Fail to Write " + mEncryptionDexFile);
+                Log.d(Log.TAG, "Fail to Write " + mEncryptionJarFile);
                 System.exit(0);
             }
-            Class localClass = Class.forName("android.app.ActivityThread");
-            Object localObject1 = localClass.getMethod("currentActivityThread",
-                    new Class[0]).invoke(null, new Object[0]);
-            Field localField1 = localClass.getDeclaredField("mPackages");
-            localField1.setAccessible(true);
-            Object localObject2 = localField1.get(localObject1);
-            localField1.setAccessible(false);
-            Method localMethod = localObject2.getClass().getMethod("get",
-                    new Class[] { Object.class });
-            Object[] arrayOfObject = new Object[1];
-            arrayOfObject[0] = getPackageName();
-            Object localObject3 = ((WeakReference) localMethod.invoke(
-                    localObject2, arrayOfObject)).get();
-            Field localField2 = localObject3.getClass().getDeclaredField(
-                    "mClassLoader");
-            mDexClassLoader = new DexClassLoader(dexPath, odexPath, libPath,
-                    getClassLoader());
-            localField2.setAccessible(true);
-            localField2.set(localObject3, mDexClassLoader);
-            localField2.setAccessible(false);
-            return;
+            Class classActivityThread = Class
+                    .forName("android.app.ActivityThread");
+            Method methodCurrentActivityThread = classActivityThread
+                    .getMethod("currentActivityThread");
+
+            Object objectActivityThread = methodCurrentActivityThread
+                    .invoke(null);
+
+            Field fieldMPackages = classActivityThread
+                    .getDeclaredField("mPackages");
+            fieldMPackages.setAccessible(true);
+            Object loadedApks = fieldMPackages.get(objectActivityThread);
+            fieldMPackages.setAccessible(false);
+
+            Method methodGet = loadedApks.getClass().getMethod("get",
+                    Object.class);
+            WeakReference wr = (WeakReference) methodGet.invoke(loadedApks,
+                    getPackageName());
+
+            Object objLoadedApk = wr.get();
+            Class classLoadedApk = objLoadedApk.getClass();
+            Field fieldMClassLoader = classLoadedApk
+                    .getDeclaredField("mClassLoader");
+            fieldMClassLoader.setAccessible(true);
+            DexClassLoader loader = new DexClassLoader(dexPath, odexPath, libPath, getClassLoader());
+            fieldMClassLoader.set(objLoadedApk, loader);
+            fieldMClassLoader.setAccessible(false);
         } catch (Exception e) {
             Log.e(Log.TAG, "error : " + e);
         }
@@ -231,10 +136,11 @@ public class LoaderApp extends Application {
 
     private String generateSrcDex() {
         try {
-            String srcDexPath = getDir("dex_path", MODE_PRIVATE)
-                    .getAbsolutePath() + File.separator + mEncryptionDexFile;
+            deleteFile(APP_DEX_PATH);
+            String srcDexPath = getDir(APP_DEX_PATH, MODE_PRIVATE)
+                    .getAbsolutePath() + File.separator + mDestJarFile;
             FileOutputStream fis = new FileOutputStream(srcDexPath);
-            InputStream is = getAssets().open(mEncryptionDexFile);
+            InputStream is = getAssets().open(mEncryptionJarFile);
             byte buffer[] = new byte[4096];
             int read = 0;
             while ((read = is.read(buffer)) > 0) {
@@ -262,7 +168,7 @@ public class LoaderApp extends Application {
                 if (eventType == XmlResourceParser.START_TAG) {
                     strName = xmlParser.getName();
                     if ("dexfile".equalsIgnoreCase(strName)) {
-                        mEncryptionDexFile = xmlParser.nextText();
+                        mEncryptionJarFile = xmlParser.nextText();
                     }
                 }
                 eventType = xmlParser.next();
