9329514ad88c4fa910cab96f45b77d88135ad399
taugin <taugin@163.com>
2015-05-30 17:16:51 +0800

    1. 添加WrapperApp和DexUtil文件

---
+++ src/com/loader/dexloader/DexUtil.java
@@ -0,0 +1,127 @@
+package com.loader.dexloader;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.res.XmlResourceParser;
+import android.util.Base64;
+
+public class DexUtil {
+    private static final String LODER_CONFIG_FILE = "loaderconfig.dat";
+
+    public static final String APPLICATION_KEY = "APPLICATION_CLASS_NAME";
+    public static final String APP_DEX_PATH = "dex_path";
+
+    private String mEncryptionJarFile = "_dex_data";
+    private String mDestJarFile = "_dex_data.jar";
+
+    private Context mContext;
+
+    public DexUtil(Context context) {
+        mContext = context;
+        parseLoaderConfig();
+    }
+
+    private void parseLoaderConfig() {
+        try {
+            InputStream is = mContext.getAssets().open(LODER_CONFIG_FILE);
+            // is = decodeString(is);
+            if (is != null) {
+                parseXml(is);
+                is.close();
+            }
+        } catch (IOException e) {
+            Log.d(Log.TAG, "error : " + e);
+        }
+    }
+
+    public String extractJarFile() {
+        try {
+            // mContext.deleteFile(APP_DEX_PATH);
+            String srcDexPath = mContext.getDir(APP_DEX_PATH, Application.MODE_PRIVATE)
+                    .getAbsolutePath() + File.separator + mDestJarFile;
+            InputStream is = mContext.getAssets().open(mEncryptionJarFile);
+            File file = new File(srcDexPath);
+            if (file.exists()) {
+                if (file.length() == is.available()) {
+                    Log.d(Log.TAG, mDestJarFile + " is exsit ...");
+                    is.close();
+                    return srcDexPath;
+                }
+            }
+            Log.d(Log.TAG, "Copy dexfile ...");
+
+            FileOutputStream fis = new FileOutputStream(srcDexPath);
+            byte buffer[] = new byte[4096];
+            int read = 0;
+            while ((read = is.read(buffer)) > 0) {
+                fis.write(buffer, 0, read);
+            }
+            is.close();
+            fis.close();
+            return srcDexPath;
+        } catch (IOException e) {
+            Log.d(Log.TAG, "error : " + e);
+        }
+        return null;
+    }
+
+    private void parseXml(InputStream in) {
+        int eventType;
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            factory.setNamespaceAware(true);
+            XmlPullParser xmlParser = factory.newPullParser();
+            xmlParser.setInput(in, "UTF-8");
+            eventType = xmlParser.getEventType();
+            String strName = null;
+            while (eventType != XmlResourceParser.END_DOCUMENT) {
+                if (eventType == XmlResourceParser.START_TAG) {
+                    strName = xmlParser.getName();
+                    if ("dexfile".equalsIgnoreCase(strName)) {
+                        mEncryptionJarFile = xmlParser.nextText();
+                    }
+                }
+                eventType = xmlParser.next();
+            }
+        } catch (XmlPullParserException e) {
+            Log.d(Log.TAG, "error : " + e);
+        } catch (IOException e) {
+            Log.d(Log.TAG, "error : " + e);
+        }
+    }
+
+    private InputStream decodeString(InputStream is) {
+        if (is == null) {
+            return null;
+        }
+        try {
+            byte[] buffer = new byte[1024];
+            int read = -1;
+            StringBuilder builder = new StringBuilder();
+            while ((read = is.read(buffer)) > 0) {
+                builder.append(new String(buffer, 0, read, "UTF-8"));
+            }
+            is.close();
+            if (builder.length() > 0) {
+                byte[] decodedString = Base64.decode(builder.toString(),
+                        Base64.DEFAULT);
+                InputStream inputStream = new ByteArrayInputStream(
+                        decodedString);
+                return inputStream;
+            }
+        } catch (IOException e) {
+            Log.d(Log.TAG, "error : " + e);
+        }
+        return null;
+    }
+}
--- src/com/loader/dexloader/LoaderApp.java
+++ src/com/loader/dexloader/LoaderApp.java
@@ -1,34 +1,18 @@
 package com.loader.dexloader;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.lang.ref.WeakReference;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-import org.xmlpull.v1.XmlPullParserFactory;
 
 import android.app.Application;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
-import android.content.res.XmlResourceParser;
 import android.text.TextUtils;
-import android.util.Base64;
 import dalvik.system.DexClassLoader;
 
 public class LoaderApp extends Application {
 
-    private static final String APPLICATION_KEY = "APPLICATION_CLASS_NAME";
-    private static final String LODER_CONFIG_FILE = "loaderconfig.dat";
-    private static final String APP_DEX_PATH = "dex_path";
-    private String mEncryptionJarFile = "_dex_data";
-    private String mDestJarFile = "_dex_data.jar";
     private Application mApp;
 
     public void onCreate() {
@@ -36,7 +20,7 @@ public class LoaderApp extends Application {
         try {
             String str2 = getPackageManager().getApplicationInfo(
                     getPackageName(), PackageManager.GET_META_DATA).metaData
-                    .getString(APPLICATION_KEY);
+                    .getString(DexUtil.APPLICATION_KEY);
             mApp = ((Application) getClassLoader().loadClass(str2)
                     .newInstance());
             Method localMethod = Class
@@ -56,16 +40,16 @@ public class LoaderApp extends Application {
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         try {
-            String odexPath = getDir(APP_DEX_PATH, MODE_PRIVATE)
+            String odexPath = getDir(DexUtil.APP_DEX_PATH, MODE_PRIVATE)
                     .getAbsolutePath();
-            parseLoaderConfig();
-            String dexPath = generateSrcDex();
+            DexUtil util = new DexUtil(this);
+            String dexPath = util.extractJarFile();
             String libPath = getApplicationInfo().nativeLibraryDir;
             Log.d(Log.TAG, "dexPath : " + dexPath);
             Log.d(Log.TAG, "libPath : " + libPath);
             Log.d(Log.TAG, "odexPath : " + odexPath);
             if (TextUtils.isEmpty(dexPath)) {
-                Log.d(Log.TAG, "Fail to Write " + mEncryptionJarFile);
+                Log.d(Log.TAG, "Fail to Generate jar file ");
                 System.exit(0);
             }
             Class classActivityThread = Class
@@ -119,89 +103,5 @@ public class LoaderApp extends Application {
         if (mApp != null) {
             mApp.onTerminate();
         }
-    }
-
-    private void parseLoaderConfig() {
-        try {
-            InputStream is = getAssets().open(LODER_CONFIG_FILE);
-            // is = decodeString(is);
-            if (is != null) {
-                parseXml(is);
-                is.close();
-            }
-        } catch (IOException e) {
-            Log.d(Log.TAG, "error : " + e);
-        }
-    }
-
-    private String generateSrcDex() {
-        try {
-            deleteFile(APP_DEX_PATH);
-            String srcDexPath = getDir(APP_DEX_PATH, MODE_PRIVATE)
-                    .getAbsolutePath() + File.separator + mDestJarFile;
-            FileOutputStream fis = new FileOutputStream(srcDexPath);
-            InputStream is = getAssets().open(mEncryptionJarFile);
-            byte buffer[] = new byte[4096];
-            int read = 0;
-            while ((read = is.read(buffer)) > 0) {
-                fis.write(buffer, 0, read);
-            }
-            is.close();
-            fis.close();
-            return srcDexPath;
-        } catch (IOException e) {
-            Log.d(Log.TAG, "error : " + e);
-        }
-        return null;
-    }
-
-    private void parseXml(InputStream in) {
-        int eventType;
-        try {
-            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-            factory.setNamespaceAware(true);
-            XmlPullParser xmlParser = factory.newPullParser();
-            xmlParser.setInput(in, "UTF-8");
-            eventType = xmlParser.getEventType();
-            String strName = null;
-            while (eventType != XmlResourceParser.END_DOCUMENT) {
-                if (eventType == XmlResourceParser.START_TAG) {
-                    strName = xmlParser.getName();
-                    if ("dexfile".equalsIgnoreCase(strName)) {
-                        mEncryptionJarFile = xmlParser.nextText();
-                    }
-                }
-                eventType = xmlParser.next();
-            }
-        } catch (XmlPullParserException e) {
-            Log.d(Log.TAG, "error : " + e);
-        } catch (IOException e) {
-            Log.d(Log.TAG, "error : " + e);
-        }
-    }
-
-    private InputStream decodeString(InputStream is) {
-        if (is == null) {
-            return null;
-        }
-        try {
-            byte[] buffer = new byte[1024];
-            int read = -1;
-            StringBuilder builder = new StringBuilder();
-            while ((read = is.read(buffer)) > 0) {
-                builder.append(new String(buffer, 0, read, "UTF-8"));
-            }
-            is.close();
-            if (builder.length() > 0) {
-                byte[] decodedString = Base64.decode(builder.toString(),
-                        Base64.DEFAULT);
-                InputStream inputStream = new ByteArrayInputStream(
-                        decodedString);
-                return inputStream;
-            }
-        } catch (IOException e) {
-            Log.d(Log.TAG, "error : " + e);
-        }
-        return null;
     }
 }
